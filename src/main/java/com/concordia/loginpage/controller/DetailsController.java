25ad989552a0c8aab7d81b1c21cb97cc74bebf09
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-10-05 19:19:17 -0400

    Connect details page to controller

--- src/main/java/com/concordia/loginpage/controller/DetailsController.java
+++ src/main/java/com/concordia/loginpage/controller/DetailsController.java
@@ -1,4 +1,42 @@
 package com.concordia.loginpage.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class DetailsController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label ageLabel;
+
+    @FXML
+    private Label professionLabel;
+
+    @FXML
+    private Label twitterLabel;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label websiteLabel;
+
+    @FXML
+    void initialize() {
+        ageLabel.setText("25");
+    }
 }
